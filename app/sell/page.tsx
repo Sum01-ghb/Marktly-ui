b44eca2dc0c4ec61ba5329aaf3f58582a5b7add9
Sumit <sumit500241@gmail.com>
2025-07-23 22:07:17 +0530

    Setup sell route and file uploads

---
+++ app/sell/page.tsx
@@ -0,0 +1,71 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SelectCategory } from "../components/SelectCategory";
+import { Textarea } from "@/components/ui/textarea";
+import { TipTapEditor } from "../components/Editor";
+import { UploadDropzone } from "../lib/uploadthing";
+import { Button } from "@/components/ui/button";
+
+export default function SellRoute() {
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8 mb-14">
+      <Card>
+        <form>
+          <CardHeader>
+            <CardTitle>Sell your product with ease</CardTitle>
+            <CardDescription>
+              Please describe your product here in detail so that it can be sold
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-y-10">
+            <div className="flex flex-col gap-y-2">
+              <Label>Name</Label>
+              <Input type="text" placeholder="Name of your Product" />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Category</Label>
+              <SelectCategory />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Price</Label>
+              <Input placeholder="Enter price in $" type="number" />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Small Summary</Label>
+              <Textarea placeholder="Describe your product shortly right here..." />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Description</Label>
+              <TipTapEditor />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Product Images</Label>
+              <UploadDropzone endpoint="imageUploader" />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Product File</Label>
+              <UploadDropzone endpoint="productFileUpload" />
+            </div>
+          </CardContent>
+          <CardFooter className="mt-5">
+            <Button>Submit Form</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </section>
+  );
+}
